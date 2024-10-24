a26f0c7241d868c4fb9d015443c5a40b8bdd4c3d
vralov <frolovmakar247@gmail.com>
2024-10-24 18:59:10 +0300

    Update Фролов, Расчет количества книг на дискете.py

--- Лабораторная работа 1/Фролов, Расчет количества книг на дискете.py
+++ Лабораторная работа 1/Фролов, Расчет количества книг на дискете.py
@@ -1,12 +1,14 @@
 # TODO Найдите количество книг, которое можно разместить на дискете
-stranic = 100
-strok = 50
-simbol = 25
-simvolume = 4
-disceta_volume = 1.44 * 1024 * 1024
+import math
+page = 100
+line = 50
+symbol = 25
+symbol_volume = 4
+diskette_volume = 1.44 * 1024 * 1024
 
-book_volume = stranic*strok*simbol*simvolume
+book_volume = page * line * symbol * symbol_volume
 
-amountofbooks = disceta_volume//book_volume
+amount_of_books = diskette_volume / book_volume
 
-print("Количество книг, помещающихся на дискету:", int(amountofbooks))
+print("Количество книг, помещающихся на дискету:", math.floor(amount_of_books))
+
