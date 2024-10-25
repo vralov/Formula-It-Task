4fd65574a2395d370784634ad8a4696ae2890653
vralov <frolovmakar247@gmail.com>
2024-10-25 21:44:20 +0300

    Update Фролов, Замена пропущенного элемента средним арифметическим.py

--- Лабораторная работа 1/Фролов, Замена пропущенного элемента средним арифметическим.py
+++ Лабораторная работа 1/Фролов, Замена пропущенного элемента средним арифметическим.py
@@ -3,10 +3,9 @@ numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -
 # TODO заменить значение пропущенного элемента средним арифметическим
 for i in range(0, len(numbers)):
     if numbers[i] is None:
-        S = 0
+        total = 0
         numbers[i] = 0
         for t in range(0, len(numbers)):
-            S = S + numbers[t]
-        numbers[i] = S / len(numbers)
+            total = total + numbers[t]
+        numbers[i] = total / len(numbers)
 print("Измененный список:", numbers)
-
